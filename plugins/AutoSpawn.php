aeff945f6de78eaadee52fb09d0265fd73c21c3e
키리토 <nno88551@naver.com>
2014-02-09 22:42:56 +0900

    [수정]몬스터 스폰 자표 안정화

--- plugins/AutoSpawn.php
+++ plugins/AutoSpawn.php
@@ -23,7 +23,7 @@ class MilkRPG implements Plugin{
 	public function init(){
 		$this->api->addHandler("entity.death", array($this, "han"));
 		$this->conf = new Config($this->api->plugin->configPath($this)."config.yml",CONFIG_YAML, array(
-			"스폰시간" => 30,//20이 1초
+			"스폰시간" => 15,//20이 1초
 			"몬스터최대수" => 25,
 		));
 		$this->api->schedule($this->conf->get("스폰시간"), array($this, "han"), array(), true, "Monster.spawn");
@@ -34,28 +34,38 @@ class MilkRPG implements Plugin{
 			case "Monster.Spawn":
 				foreach($this->api->level->getAll as $level){
 					if($this->api->time->getPhase($level) == "night"){//밤일시에 몬스터 스폰
-						if($this->spawn < $this->conf->get("몬스터최대수") and mt_rand(0,6) == mt_rand(0,6)){
+						if($this->spawn < $this->conf->get("몬스터최대수")){
 							$mobrand = mt_rand(32,36);
 							$data = array(
 								"x" => mt_rand(0,25580)/100,
-								"y" => mt_rand(64,100),//이부분은 현재 베타
+								"y" => mt_rand(0,12700)/100,
 								"z" => mt_rand(0,25580)/100,
 							);
-							$this->spawn++;
-							$e = $this->api->entity->add($this->api->level->getDefault(), ENTITY_MOB, $mobrand, $data);
-							$this->api->entity->spawnToAll($e);
+							$block = $level->getBlock(new Vector3($data["x"],$data["y"],$data["z"]));
+							$blockd = $level->getBlock(new Vector3($data["x"],$data["y"]-1,$data["z"]));
+							if(($block->getID() == 0 or !$block->isFullBlock)
+							and $blockd->getID() != 0 and $blockd->isFullBlock){
+								$this->spawn++;
+								$e = $this->api->entity->add($level, ENTITY_MOB, $mobrand, $data);
+								$this->api->entity->spawnToAll($e);
+							}
 						}
 					}
-					if($this->spawn < $this->conf->get("몬스터최대수") and mt_rand(0,6) == mt_rand(0,6)){
+					if($this->spawn < $this->conf->get("몬스터최대수")){
 						$mobrand = mt_rand(10,13);
 						$data = array(
 							"x" => mt_rand(0,25580)/100,
-							"y" => mt_rand(64,100),//이부분은 현재 베타
+							"y" => mt_rand(0,12700)/100
 							"z" => mt_rand(0,25580)/100,
 						);
-						$this->spawn++;
-						$e = $this->api->entity->add($this->api->level->getDefault(), ENTITY_MOB, $mobrand, $data);
-						$this->api->entity->spawnToAll($e);
+						$block = $level->getBlock(new Vector3($data["x"],$data["y"],$data["z"]));
+						$blockd = $level->getBlock(new Vector3($data["x"],$data["y"]-1,$data["z"]));
+						if(($block->getID() == 0 or !$block->isFullBlock)
+						and $blockd->getID() != 0 and $blockd->isFullBlock){
+							$this->spawn++;
+							$e = $this->api->entity->add($level, ENTITY_MOB, $mobrand, $data);
+							$this->api->entity->spawnToAll($e);
+						}
 					}
 				break;
 			case "entity.death":
